c3af931026c12670287a692f7d21b3e2def16868
princecowslip <hfiver6@gmail.com>
2026-05-21 23:44:41 +1000

    Do

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -63,10 +62,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 
     buildFeatures {
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@
 
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
 }
 
 // No further configuration at root scope.
